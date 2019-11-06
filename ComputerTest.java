3108452f36187b40570d7092463ebe9aada54034
Thaddaeus <tadhug@gmail.com>
2019-11-06 10:27:17 -0800

    added skeleton for ComputerTest class

---
+++ ComputerTest.java
@@ -0,0 +1,95 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Thaddaeus
+ *
+ */
+class ComputerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link Computer#Computer()}.
+	 */
+	@Test
+	final void testComputer() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Computer#compile(java.util.List)}.
+	 */
+	@Test
+	final void testCompile() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Computer#execute()}.
+	 */
+	@Test
+	final void testExecute() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Computer#executeAdd()}.
+	 */
+	@Test
+	final void testExecuteAdd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Computer#executeAddU()}.
+	 */
+	@Test
+	final void testExecuteAddU() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Computer#executeAnd()}.
+	 */
+	@Test
+	final void testExecuteAnd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Computer#executeOr()}.
+	 */
+	@Test
+	final void testExecuteOr() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Computer#executeORI(java.lang.String, int)}.
+	 */
+	@Test
+	final void testExecuteORI() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link Computer#createMap()}.
+	 */
+	@Test
+	final void testCreateMap() {
+		fail("Not yet implemented");
+	}
+
+}
